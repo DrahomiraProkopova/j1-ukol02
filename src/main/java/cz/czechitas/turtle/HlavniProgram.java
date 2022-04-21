4a85bdcb32cb8cc12a3774e95c9adef46eae4b39
Drahomira Prokopova <prokopova.drahomira@gmail.com>
2022-04-21 17:43:41 +0200

    Vytvoreny parametrizovane metody (trojuhelnik, ctverec, obdelnik, kolecko)

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,12 +2,61 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     public void main(String[] args) {
         Turtle zofka;
-
         zofka = new Turtle();
+        zofka.setPenWidth(2);
+
+    }
+
+    private void nakresliKolecko(Turtle zofka, Color barvaCary, double polomer) {
+        zofka.setPenColor(barvaCary);
+        double polomerA = Math.pow(polomer, 2);        //Pythagorova veta
+        double delkaStrany = (polomerA + polomerA);  //Pythagorova veta
+        for (int i = 0; i < 45; i++) {    //i < 360/uhel
+            zofka.move(delkaStrany);
+            zofka.turnRight(8);   //kolecko bude 45uhelnik
+        }
+
+
+    }
+     /*
+    private void nakresliKolecko(Turtle zofka, Color barvaCary, double delkaStrany) {
+        zofka.setPenColor(barvaCary);
+        for (int i = 0; i < 360; i++) {
+            zofka.move(delkaStrany);
+            zofka.turnRight(1);
+        }     */                    //druha varianta nakresleni kolecka
+
+
+    private void nakresliObdelnik(Turtle zofka, Color barvaCary, double delkaStranyA, double delkaStranyB) {
+        for (int i = 0; i < 2; i++) {
+            zofka.setPenColor(barvaCary);
+            zofka.move(delkaStranyA);
+            zofka.turnRight(90);
+            zofka.move(delkaStranyB);
+            zofka.turnRight(90);
+        }
+    }
+
+    private void nakresliRovnostrannyTrojuhelnik(Turtle zofka, Color barvaCary, double delkaStrany) {
+        for (int i = 0; i < 3; i++) {
+            zofka.setPenColor(barvaCary);
+            zofka.move(delkaStrany);
+            zofka.turnRight(120);
+        }
+    }
+
+    private void nakresliCtverec(Turtle zofka, Color barvaCary, double delkaStrany) {
+        for (int i = 0; i < 4; i++) {
+            zofka.setPenColor(barvaCary);
+            zofka.move(delkaStrany);
+            zofka.turnRight(90);
+        }
     }
 
 }
