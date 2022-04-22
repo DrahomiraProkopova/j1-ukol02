96a112cf842b241c23c32f61349adb1b1054e0fe
Drahomira Prokopova <prokopova.drahomira@gmail.com>
2022-04-22 10:52:30 +0200

    Cast 2. snehulak, zmrzlina a masinka

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -9,45 +9,96 @@ public class HlavniProgram {
     public void main(String[] args) {
         Turtle zofka;
         zofka = new Turtle();
-        zofka.setPenWidth(2);
+        zofka.setPenWidth(5);
 
+        Turtle jana;
+        jana = new Turtle();
+        jana.setPenWidth(5);
+
+        Turtle pepa;
+        pepa = new Turtle();
+        pepa.setPenWidth(5);
+
+
+        nakresliZmrzlinu(zofka, 500, 150);
+        nakresliSnehulaka(jana, 200, 200);
+        nakresliMasinku(pepa, 800, 200);
 
     }
 
-    private void nakresliKolecko(Turtle zofka, Color barvaCary, double polomer) {
-        zofka.setPenColor(barvaCary);
+    private void nakresliMasinku(Turtle turtle, double X, double Y) {
+        turtle.setLocation(X, Y);
+        nakresliKolecko(turtle, Color.black, 40);
+        nakresliObdelnik(turtle, Color.orange, 100, 80);
+        turtle.turnLeft(90);
+        nakresliObdelnik(turtle, Color.orange, 120, 70);
+        turtle.setLocation(X - 40, Y + 40);
+        nakresliKolecko(turtle, Color.black, 20);
+        turtle.setLocation(X - 90, Y + 40);
+        nakresliKolecko(turtle, Color.black, 20);
+        turtle.setLocation(X - 120, Y - 50);
+        turtle.turnLeft(120);
+        nakresliRovnostrannyTrojuhelnik(turtle, Color.orange, 90);
+    }
+
+    private void nakresliSnehulaka(Turtle turtle, double X, double Y) {
+        turtle.setLocation(X, Y);
+        nakresliKolecko(turtle, Color.blue, 40);
+        turtle.setLocation(X + 10, Y - 70);
+        nakresliKolecko(turtle, Color.blue, 30);
+        turtle.setLocation(X + 20, Y - 120);
+        nakresliKolecko(turtle, Color.blue, 20);
+        turtle.setLocation(X - 10, Y - 65);
+        nakresliKolecko(turtle, Color.blue, 10);
+        turtle.setLocation(X + 70, Y - 65);
+        nakresliKolecko(turtle, Color.blue, 10);
+    }
+
+    private void nakresliZmrzlinu(Turtle turtle, double X, double Y) {
+        turtle.setLocation(X, Y);
+        turtle.turnRight(180);
+        nakresliKolecko(turtle, Color.red, 60);
+        turtle.setLocation(X, Y + 20);
+        nakresliRovnostrannyTrojuhelnik(turtle, Color.black, 120);
+        turtle.setLocation(X, Y);
+    }
+
+
+    private void nakresliKolecko(Turtle turtle, Color barvaCary, double polomer) {
+        turtle.setPenColor(barvaCary);
         double obvod = (2 * (Math.PI) * polomer);
         for (int i = 0; i < 45; i++) {    //i < 360/uhel
-            zofka.move(obvod / 45);
-            zofka.turnRight(8);   //kolecko bude 45uhelnik
+            turtle.move(obvod / 45);
+            turtle.turnRight(8);   //kolecko bude 45uhelnik
         }
 
     }
 
-    private void nakresliObdelnik(Turtle zofka, Color barvaCary, double delkaStranyA, double delkaStranyB) {
+
+    private void nakresliObdelnik(Turtle turtle, Color barvaCary, double delkaStranyA, double delkaStranyB) {
         for (int i = 0; i < 2; i++) {
-            zofka.setPenColor(barvaCary);
-            zofka.move(delkaStranyA);
-            zofka.turnRight(90);
-            zofka.move(delkaStranyB);
-            zofka.turnRight(90);
+            turtle.setPenColor(barvaCary);
+            turtle.move(delkaStranyA);
+            turtle.turnRight(90);
+            turtle.move(delkaStranyB);
+            turtle.turnRight(90);
         }
     }
 
-    private void nakresliRovnostrannyTrojuhelnik(Turtle zofka, Color barvaCary, double delkaStrany) {
-        zofka.turnRight(30);
+    private void nakresliRovnostrannyTrojuhelnik(Turtle turtle, Color barvaCary, double delkaStrany) {
+        turtle.turnRight(30);
         for (int i = 0; i < 3; i++) {
-            zofka.setPenColor(barvaCary);
-            zofka.move(delkaStrany);
-            zofka.turnRight(120);
+            turtle.setPenColor(barvaCary);
+            turtle.move(delkaStrany);
+            turtle.turnRight(120);
         }
     }
 
-    private void nakresliCtverec(Turtle zofka, Color barvaCary, double delkaStrany) {
+    private void nakresliCtverec(Turtle turtle, Color barvaCary, double delkaStrany) {
         for (int i = 0; i < 4; i++) {
-            zofka.setPenColor(barvaCary);
-            zofka.move(delkaStrany);
-            zofka.turnRight(90);
+            turtle.setPenColor(barvaCary);
+            turtle.move(delkaStrany);
+            turtle.turnRight(90);
         }
     }
 
